ff422b97d7f4157d3bedb1af2ece0ce4369626e3
AnneKDesler <s194333@dtu.dk>
2023-01-03 13:18:18 +0100

    Final exercise uge 1

--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -6,7 +6,8 @@ import click
 
 from data import mnist
 from model import MyAwesomeModel
-
+from torch import nn, optim
+import matplotlib.pyplot as plt
 
 @click.group()
 def cli():
@@ -22,6 +23,36 @@ def train(lr):
     # TODO: Implement training loop here
     model = MyAwesomeModel()
     train_set, _ = mnist()
+    epochs = 3
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
+    criterion = nn.NLLLoss()
+    steps = 0
+    running_loss = 0
+    print_every = 60
+    losses = []
+    for e in range(epochs):
+        # Model in training mode, dropout is on
+        model.train()
+        print("Epoch: {}/{}.. ".format(e+1, epochs))
+        for images, labels in train_set:
+            steps += 1
+            
+            # Flatten images into a 784 long vector
+            images.resize_(images.size()[0], 784)
+            
+            optimizer.zero_grad()
+            
+            output = model.forward(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+            
+            losses.append(loss.item())
+    torch.save(model, 'C:/Users/anned/OneDrive - Danmarks Tekniske Universitet/Uni/MLOps/dtu_mlops/s1_development_environment/exercise_files/final_exercise/checkpoint.pth')
+    plt.plot(losses)
+    plt.ylabel('Train loss')
+    plt.xlabel('Train loss')
+    plt.show()
 
 
 @click.command()
@@ -33,6 +64,23 @@ def evaluate(model_checkpoint):
     # TODO: Implement evaluation logic here
     model = torch.load(model_checkpoint)
     _, test_set = mnist()
+    accuracy = 0
+    for images, labels in test_set:
+
+        images = images.resize_(images.size()[0], 784)
+
+        output = model(images)
+
+        ## Calculating the accuracy 
+        # Model's output is log-softmax, take exponential to get the probabilities
+        ps = torch.exp(output)
+        # Class with highest probability is our predicted class, compare with true label
+        equality = (labels.data == ps.max(1)[1])
+        # Accuracy is number of correct predictions divided by all predictions, just take the mean
+        accuracy += equality.type_as(torch.FloatTensor()).mean()
+
+    accuracy /= len(test_set)
+    print(accuracy.item())
 
 
 cli.add_command(train)
